e2d250c9e5739059fb0e6018baad14835a88bcb9
Soy Miguel <magutierrezhijosa@gmail.com>
2026-03-17 20:18:36 +0100

    Prueba de si tiene anti-bot la paginna

---
+++ config.py
@@ -0,0 +1,7 @@
+
+
+
+URL_TO_SCRAP = "https://cmfdoc.funchal.pt/publicitações"
+
+ELEMENT_SCRAP_LOCATOR = "subCategory"
+
---
+++ main.py
@@ -0,0 +1,20 @@
+# Importamos playright
+from playwright.sync_api import sync_playwright
+
+
+
+
+    # Creamos el contxt manager 
+with sync_playwright() as p:
+
+    browser = p.chromium.launch(headless=False)
+
+    page = browser.new_page()
+
+    page.goto("https://cmfdoc.funchal.pt/publicitações")
+
+    print(page.title())
+
+    browser.close()
+
+
