56b3df2b0a7781e9c513f3e3252e429d8035dd9e
Soy Miguel <magutierrezhijosa@gmail.com>
2026-03-24 20:37:24 +0100

    Declaracion del bucle para recoger los enlaces PDF

---
+++ paso2_categoria.py
@@ -0,0 +1,53 @@
+
+# paso2_categoria.py
+import requests
+from bs4 import BeautifulSoup
+from config import BASE_URL,HEADERS
+
+# Usamos Cemiterior como categoria de prueba 
+URL_CATEGORIA = "https://cmfdoc.funchal.pt/publicitações/mm-editais-cemiterios.html"
+
+# Descargamos la pagina de la categoria
+respuesta = requests.get(URL_CATEGORIA, headers=HEADERS)
+
+# Comprobamos que la solicitud esta bien co u prit del status_code
+print(f"Codigo de respuesta: {respuesta.status_code}")
+
+# Paso para transformar el HTML en ua estructura de arbol avegable
+soup = BeautifulSoup(respuesta, "html.parser") 
+
+# Declaramos la variable para guardar las publicaciones
+publicaciones = []
+
+# La estructura HTML de cada publicacion es :
+#  1. <a href="enlace_al_pdf"><img...></a> =====> enlace al PDF
+#  2. <h>Titulo de la publicacio</h4>  =========>  titulo
+#  3. Fecha e texto suelto   ===================> fecha
+
+# Buscamos todos los <h4> - cada uno es una publicacion 
+for h4 in soup.find_all("h4"):
+
+    # Recogemos el valor del titulo 
+    titulo = h4.get_text(strip=True)
+
+    # El PDF esta en el <a> que hay ANTES del <h4>
+    # previous_sibling navega hacia atras en el HTML
+    pdf_link = None
+
+    # Bucle para recoger los enlaces al PDF
+    # Iteramos hacia atras el arbol HTML desde el <h4>
+    for hermano_a_pdf in h4.previous_siblings:
+
+        # Cada iteracion se reinicia la variable a
+        a = None
+
+        # El hermano puede ser directamente un <a> o contenerlo
+        if hermano_a_pdf.name == "a":
+
+            a = hermano_a_pdf
+
+        elif hasattr(hermano_a_pdf , "find"):
+
+            a = hermano_a_pdf.find("a")
+
+        
